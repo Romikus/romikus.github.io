bb250490534f27430e7e437044e0dd271984bf3f
Roman Naumov <romas.naumov@gmail.com>
2017-04-12 23:53:10 +0300

    Проект Emfy

--- Emfy/js/script.js
+++ Emfy/js/script.js
@@ -1,5 +1,6 @@
 var navMain = document.querySelector('.main-menu');
 var navToggle = document.querySelector('.main-menu__toggle');
+var menuList = document.querySelector('.main-menu__list');
 
 navMain.classList.remove('main-menu--nojs');
 
@@ -12,3 +13,23 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-menu--opened');
   }
 });
+
+menuList.onclick = function(event) {
+  var target = event.target;
+
+  if(!target.nextElementSibling) {
+    return;
+  }
+  if (target.nextElementSibling.tagName != 'DIV') {
+    return;
+  }
+  show(target.nextElementSibling)
+}
+
+function show(selectedDiv) {
+  if (selectedDiv.classList.contains('submenu--closed')) {
+    selectedDiv.classList.remove('submenu--closed');
+  } else {
+    selectedDiv.classList.add('submenu--closed');
+  }
+}
